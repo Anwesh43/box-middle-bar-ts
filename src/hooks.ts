bfdb08ea2fa8d9cf47eb5d4de1829fa9119eb12f
Anwesh43 <anweshthecool0@gmail.com>
2024-01-21 15:45:11 +0530

    useDimension hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -24,3 +24,22 @@ export const useAnimatedScale = (scGap : number = 0.01, delay : number = 20) => 
         }
     }
 }
+
+export const useDimension = () => {
+    const [w, setW] = useState(window.innerWidth)
+    const [h, setH] = useState(window.innerHeight)
+    useEffect(() => {
+        const resizeListener = () => {
+            setW(window.innerWidth)
+            setH(window.innerHeight)
+        }
+        window.addEventListener('resize', resizeListener, false)
+        return () => {
+            window.removeEventListener('resize', resizeListener)
+        }
+    })
+    return {
+        w, 
+        h
+    }
+}
