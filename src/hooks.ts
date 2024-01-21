a9e496a5b8da5d3b77fea0b1a8fe5af729c7df11
Anwesh43 <anweshthecool0@gmail.com>
2024-01-21 15:55:02 +0530

    useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 export const useAnimatedScale = (scGap : number = 0.01, delay : number = 20) => {
     const [scale, setScale] = useState(0)
@@ -41,5 +41,51 @@ export const useDimension = () => {
     return {
         w, 
         h
+    }
+}
+
+interface UseStyleProps {
+   parentStyle : () => CSSProperties,
+   boxStyle : () => CSSProperties,
+   middleBarStyle : (i : number) => CSSProperties 
+}
+
+export const useStyle = (w : number, h : number, scale : number) : UseStyleProps  => {
+    const size : number = Math.min(w, h) / 10
+    const barW : number = size / 4
+    const background : string = "indigo"
+    const middleBarBackground : string = "#212121"
+    const position = 'absolute'
+    return {
+        parentStyle() : CSSProperties {
+            const left = `${w / 2}px`
+            const top = `${h / 2}px`
+            return {
+                position, 
+                left, 
+                top 
+            }
+        },
+        boxStyle() {
+            return {
+                position,
+                left: `${-size / 2}px`,
+                top: `${-size / 2}px`,
+                width: `${size}px`,
+                height: `${size}px`,
+                background
+            }
+        },
+        middleBarStyle(i : number) : CSSProperties {
+            return {
+                position,
+                left: `${-barW / 2}px`,
+                top: `${-size / 2}px`,
+                width: `${barW}px`,
+                height: `${size}px`,
+                transform: `rotate(${90 * i}deg)`,
+                background: middleBarBackground 
+            }
+        }
     }
 }
